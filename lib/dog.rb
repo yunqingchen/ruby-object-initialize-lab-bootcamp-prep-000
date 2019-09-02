5a2610fdd4f6a0b6426549923fbd0afefc69d5e8
Clark Chen <yyunqingchenn@gmail.com>
2019-09-02 02:55:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,7 @@
 class Dog 
   def initialize(name, breed = "Mutt")
+    @name = name 
     @breed = breed 
-  end 
-  
-  def breed-(breed)
-    @breed = breed 
-  end 
-  
-  def breed 
-    @breed
   end 
 end 
 
